ec82987c32828176e9c77bc3e620c6644a0a4211
Noam Ben Shabat <noamb@Taldor.corp>
2019-02-28 14:42:12 +0200

    Publish to npm

---
+++ angular-grid-cell.d.ts
@@ -0,0 +1,9 @@
+export { GridCellModule } from './src/app/grid-cell/grid-cell.module';
+export { ButtonColumnElement } from './src/app/grid-cell/models/classes/ButtonColumnElement';
+export { Column } from './src/app/grid-cell/models/classes/Column';
+export { DateTimeColumnElement } from './src/app/grid-cell/models/classes/DateTimeColumnElement';
+export { IconColumnElement } from './src/app/grid-cell/models/classes/IconColumnElement';
+export { ImageColumnElement } from './src/app/grid-cell/models/classes/ImageColumnElement';
+export { TextColumnElement } from './src/app/grid-cell/models/classes/TextColumnElement';
+export { ColumnAlign } from './src/app/grid-cell/models/enums/column-align.enum';
+export { MomentPipe } from './src/app/grid-cell/models/enums/moment-pipe.enum';
