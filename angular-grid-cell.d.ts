54998545eea389aa7d75a4a6557e0666fbee5ee0
Noam Ben Shabat <noamb@Taldor.corp>
2019-03-03 10:21:04 +0200

    update project settings

--- angular-grid-cell.d.ts
+++
@@ -1,9 +0,0 @@
-export { GridCellModule } from './src/app/grid-cell/grid-cell.module';
-export { ButtonColumnElement } from './src/app/grid-cell/models/classes/ButtonColumnElement';
-export { Column } from './src/app/grid-cell/models/classes/Column';
-export { DateTimeColumnElement } from './src/app/grid-cell/models/classes/DateTimeColumnElement';
-export { IconColumnElement } from './src/app/grid-cell/models/classes/IconColumnElement';
-export { ImageColumnElement } from './src/app/grid-cell/models/classes/ImageColumnElement';
-export { TextColumnElement } from './src/app/grid-cell/models/classes/TextColumnElement';
-export { ColumnAlign } from './src/app/grid-cell/models/enums/column-align.enum';
-export { MomentPipe } from './src/app/grid-cell/models/enums/moment-pipe.enum';
