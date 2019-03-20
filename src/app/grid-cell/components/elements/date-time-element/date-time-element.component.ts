ae9600671c8ca00b8e3fe38baa8fe4c4e58a375e
oferah <31686553+oferah@users.noreply.github.com>
2019-03-20 13:49:13 +0200

    fix date-time element

--- src/app/grid-cell/components/elements/date-time-element/date-time-element.component.ts
+++ src/app/grid-cell/components/elements/date-time-element/date-time-element.component.ts
@@ -1,18 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { IColumnElement } from 'src/app/grid-cell/models/interfaces/IColumnElement';
+import { ColumnElement } from 'src/app/grid-cell/models/classes/column-element';
 
 @Component({
   selector: 'tld-date-time-element',
   templateUrl: './date-time-element.component.html',
   styleUrls: ['./date-time-element.component.css']
 })
-export class DateTimeElementComponent implements OnInit {
-
-  @Input() columnElement: IColumnElement;
-  @Input() data: any;
-
-  constructor() { }
-
+export class DateTimeElementComponent extends ColumnElement implements OnInit {
   ngOnInit() {
   }
 
