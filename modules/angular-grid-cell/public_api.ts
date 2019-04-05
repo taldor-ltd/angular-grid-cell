3224de030535fafded945e9571a0b606adde079a
Noam Ben Shabat <noamb@Taldor.corp>
2019-04-05 11:30:01 +0300

    fixes #4 ...block-scoped variable 'ngDevMode'.

--- modules/angular-grid-cell/public_api.ts
+++ modules/angular-grid-cell/public_api.ts
@@ -1,12 +1,12 @@
-export * from './src/app/grid-cell/grid-cell.module';
+export { GridCellModule } from './src/app/grid-cell/grid-cell.module';
 
-export * from './src/app/grid-cell/models/classes/column';
+export { Column } from './src/app/grid-cell/models/classes/column';
 
-export * from './src/app/grid-cell/models/enums/column-align.enum';
-export * from './src/app/grid-cell/models/enums/moment-func.enum';
+export { ColumnAlign } from './src/app/grid-cell/models/enums/column-align.enum';
+export { MomentFunc } from './src/app/grid-cell/models/enums/moment-func.enum';
 
-export * from './src/app/grid-cell/components/elements/button-element/classes/button-element';
-export * from './src/app/grid-cell/components/elements/date-time-element/classes/date-time-element';
-export * from './src/app/grid-cell/components/elements/icon-element/classes/icon-element';
-export * from './src/app/grid-cell/components/elements/image-element/classes/image-element';
-export * from './src/app/grid-cell/components/elements/text-element/classes/text-element';
+export { ButtonElement } from './src/app/grid-cell/components/elements/button-element/classes/button-element';
+export { DateTimeElement } from './src/app/grid-cell/components/elements/date-time-element/classes/date-time-element';
+export { IconElement } from './src/app/grid-cell/components/elements/icon-element/classes/icon-element';
+export { ImageElement } from './src/app/grid-cell/components/elements/image-element/classes/image-element';
+export { TextElement } from './src/app/grid-cell/components/elements/text-element/classes/text-element';
