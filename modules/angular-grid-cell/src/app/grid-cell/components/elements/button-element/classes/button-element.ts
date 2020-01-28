424a430dce2da697aa2fd2f116806a830914bab3
Ofer Aharoni <ofera@Taldor.corp>
2020-01-28 12:08:11 +0200

    resolve #32 - dynamic button text

--- modules/angular-grid-cell/src/app/grid-cell/components/elements/button-element/classes/button-element.ts
+++ modules/angular-grid-cell/src/app/grid-cell/components/elements/button-element/classes/button-element.ts
@@ -3,7 +3,7 @@ import { Element } from '../../../../models/classes/element';
 export class ButtonElement extends Element {
   buttonText: string | Function;
 
-  constructor(buttonText: string | Function, buttonStyle: string, options?: Element) {
+  constructor(buttonText: string | Function, options?: Element) {
     super();
     this['type'] = 'button';
     this.buttonText = buttonText;
