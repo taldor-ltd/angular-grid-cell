d15de8c560b6ced84e3168a2c470821702d9ba84
Ofer Aharoni <ofera@taldor.co.il>
2020-11-17 12:45:05 +0200

    fix image tooltip

--- modules/angular-grid-cell/src/app/grid-cell/components/elements/image-element/image-element.component.ts
+++ modules/angular-grid-cell/src/app/grid-cell/components/elements/image-element/image-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { BaseElementComponent } from '../../../models/classes/base-element-component';
 import { ImageElement } from './classes/image-element';
 
@@ -8,6 +8,29 @@ import { ImageElement } from './classes/image-element';
   styleUrls: ['./image-element.component.css']
 })
 export class ImageElementComponent extends BaseElementComponent {
+
+  img: ElementRef;
+
   @Input() element: ImageElement;
   @Input() data: any;
+
+  @ViewChild('img', { read: ElementRef }) set content(img: ElementRef) {
+    if (img && !this.img) {
+      this.img = img;
+      setTimeout(() => {
+        if (!!this.element && !!this.element.tooltip) {
+          if (typeof this.element.tooltip === 'function') {
+            if (!!this.data) {
+              (<HTMLImageElement>this.img.nativeElement).addEventListener('mouseenter', () => this.tooltip = (<Function>this.element.tooltip)(this.data));
+            } else {
+              (<HTMLImageElement>this.img.nativeElement).addEventListener('mouseenter', () => this.tooltip = (<Function>this.element.tooltip)());
+            }
+          } else if (typeof this.element.tooltip === 'string') {
+            this.tooltip = this.element.tooltip;
+          }
+        }
+      }, 0);
+    }
+  }
+
 }
